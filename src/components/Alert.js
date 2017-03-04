232af8aaee4ace36343cc9464fcb0b7a16517e83
Martin Brychta <martin@brychta.name>
2017-03-04 18:08:10 +0100

    Autohide alert after 10 seconds

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,11 +9,20 @@ class Alert extends React.Component {
 
   handleClick = () => this.props.dismiss()
 
+  autoHide = () => {
+    this.hide = setTimeout(() => {
+      this.props.dismiss()
+      clearTimeout(this.hide)
+    }, 10000)
+  }
+
   render () {
     const { text, type } = this.props
     let className        = 'alert alert-' + type
     if (!text) {
       className += ' down'
+    } else {
+      this.autoHide()
     }
     return (
       <div className={className}>
