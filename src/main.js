b363678109e0beabc15730c7766afa2494a6949a
Martin Brychta <martin@brychta.name>
2017-03-05 15:22:24 +0100

    Add react-router

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import CSS from './stylesheets/main.scss'
 /* eslint-enable */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App'
+import Auth from './components/Auth'
 
-ReactDOM.render(<App />, document.getElementById('main'))
+function check () {
+
+}
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={App} onEnter={check} />
+    <Route path='/auth' component={Auth} />
+  </Router>
+)
+
+ReactDOM.render(router, document.getElementById('main'))
