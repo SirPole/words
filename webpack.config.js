510eab6b4af84ecc1d7cd533da77ded8eac2eba1
Martin Brychta <martin@brychta.name>
2017-03-07 19:49:25 +0100

    Optimize webpack configs, minify css

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,41 @@
 var webpack = require('webpack')
 
 var config = {
-  entry: [
+  entry   : [
     'webpack-dev-server/client?http://0.0.0.0:3000',
     './src/main.js'
   ],
-  output: {
-    filename: 'build/bundle.js'
+  output  : {
+    filename : 'build/bundle.js'
   },
-  module: {
-    loaders: [
-      { test: /\.js?$/, loaders: ['babel-loader?presets[]=react&presets[]=es2015&presets[]=stage-0'], exclude: /node_modules/ },
-      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.(woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader?name=fonts/custom/[name].[ext]' },
-      { test: /\.(png|svg)$/, loader: 'file-loader?name=img/[name].[ext]' }
+  module  : {
+    loaders : [
+      {
+        test    : /\.js?$/,
+        loaders : [ 'babel-loader?presets[]=react&presets[]=es2015&presets[]=stage-0' ],
+        exclude : /node_modules/
+      },
+      {
+        test    : /\.scss$/,
+        loaders : [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test   : /\.(woff(2)?)(\?[a-z0-9]+)?$/,
+        loader : 'file-loader?name=fonts/custom/[name].[ext]'
+      },
+      {
+        test   : /\.(png|svg)$/,
+        loader : 'file-loader?name=img/[name].[ext]'
+      }
     ]
   },
-  plugins: [
+  plugins : [
     new webpack.DefinePlugin({
-      '__DEV__': true
+      '__DEV__' : true
     })
   ]
 }
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,35 +1,59 @@
-var webpack = require('webpack')
-var ExtractTextPlugin = require("extract-text-webpack-plugin")
-var extractCSS = new ExtractTextPlugin('build/assets/css/bundle.css')
+var webpack     = require('webpack')
+var ExtractText = require('extract-text-webpack-plugin')
+var OptimizeCSS = require('optimize-css-assets-webpack-plugin')
+var extractCSS  = new ExtractText('build/assets/css/bundle.css')
 
 var config = {
-  entry: [
-    'babel-polyfill', './src/main.js'
+  entry   : [
+    'babel-polyfill',
+    './src/main.js'
   ],
-  output: {
-    filename: 'build/bundle.js'
+  output  : {
+    filename : 'build/bundle.js'
   },
-  module: {
-    loaders: [
-      { test: /\.js?$/, loaders: ['babel-loader?presets[]=react&presets[]=es2015&presets[]=stage-0'], exclude: /node_modules/ },
-      { test: /\.scss$/, loader: extractCSS.extract(['css-loader', 'sass-loader']) },
-      { test: /\.(woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader?name=build/fonts/custom/[name].[ext]' },
-      { test: /\.(png|svg)$/, loader: 'file-loader?name=img/[name].[ext]' }
+  module  : {
+    loaders : [
+      {
+        test    : /\.js?$/,
+        loaders : [ 'babel-loader?presets[]=react&presets[]=es2015&presets[]=stage-0' ],
+        exclude : /node_modules/
+      },
+      {
+        test   : /\.scss$/,
+        loader : extractCSS.extract([
+          'css-loader',
+          'sass-loader'
+        ])
+      },
+      {
+        test   : /\.(woff(2)?)(\?[a-z0-9]+)?$/,
+        loader : 'file-loader?name=build/fonts/custom/[name].[ext]'
+      },
+      {
+        test   : /\.(png|svg)$/,
+        loader : 'file-loader?name=img/[name].[ext]'
+      }
     ]
   },
-  plugins: [
+  plugins : [
     new webpack.DefinePlugin({
-      '__PROD__': true
-    }),
-    new webpack.NoErrorsPlugin(),
-    extractCSS,
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
+      'process.env' : {
+        NODE_ENV : JSON.stringify('production')
       }
     }),
-    new webpack.optimize.DedupePlugin()
+    extractCSS,
+    new OptimizeCSS({
+      assetNameRegExp     : /bundle\.css$/g,
+      cssProcessor        : require('cssnano'),
+      cssProcessorOptions : { discardComments : { removeAll : true } },
+      canPrint            : true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize : true,
+      compress : {
+        warnings : false
+      }
+    })
   ]
 }
 
